8bd915d2f2431a4ee7202d6fde7239149c96ef4c
AzizY <azizyaktubay@gmail.com>
2022-08-28 18:44:16 +0300

    creating denizli gem

---
+++ lib/denizli.rb
@@ -0,0 +1,6 @@
+require "denizli/version"
+
+module Denizli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/denizli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Denizli do
+  it "has a version number" do
+    expect(Denizli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
