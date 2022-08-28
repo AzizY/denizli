fcdb0771496d08487e93b77d59f9a0b03c6ccb67
AzizY <azizyaktubay@gmail.com>
2022-08-28 19:30:51 +0300

    creating first methods

--- lib/denizli.rb
+++ lib/denizli.rb
@@ -2,5 +2,21 @@ require "denizli/version"
 
 module Denizli
   class Error < StandardError; end
-  # Your code goes here...
+  
+  def hash_me
+    self.push(self[-1]) if self.size % 2 == 1
+    self.compact.each_slice(2).to_a.to_h
+  end
+
+  def array_me
+    self.chars
+  end
+end
+
+class Array
+  include Denizli
+end
+
+class String
+  include Denizli
 end
